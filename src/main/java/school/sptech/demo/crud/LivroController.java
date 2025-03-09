0c84b138cfeefd3a3363e89f200db69baa884ad1
FelipeGasp <felipe.gasparotto@sptech.school>
2025-03-08 22:10:04 -0300

    Feature: CRUDV1 dos livros + Entidade ORM livro

--- src/main/java/school/sptech/demo/crud/LivroController.java
+++ src/main/java/school/sptech/demo/crud/LivroController.java
@@ -15,7 +15,7 @@ public class LivroController {
     private LivroRepository repository;
 
     @GetMapping("/pesquisar")
-    public ResponseEntity listAll(){
+    public ResponseEntity<List<Livro>> listAll(){
         List<Livro> lista = repository.findAll();
         if(lista.isEmpty()){
             return ResponseEntity.status(204).build();
@@ -24,7 +24,7 @@ public class LivroController {
         }
     }
     @GetMapping("/pesquisar/{id}")
-    public ResponseEntity listById(@PathVariable Integer id){
+    public ResponseEntity<Optional<Livro>> listById(@PathVariable Integer id){
         Optional<Livro> livroEncontrado = repository.findById(id);
 
         if (livroEncontrado.isPresent()){
@@ -34,12 +34,46 @@ public class LivroController {
         return ResponseEntity.status(204).build();
     }
     @PostMapping("/criar")
-    public ResponseEntity createLivro(@RequestBody Livro livroCadastro){
+    public ResponseEntity<Livro> createLivro(@RequestBody Livro livroCadastro){
+        livroCadastro.setId(null);
         try {
             Livro livroPostado = repository.save(livroCadastro);
             return ResponseEntity.status(201).body(livroPostado);
         }catch (Exception e){
-            return ResponseEntity.status(500).body("Error: Internal Server Error, the object could´t be created");
+            return ResponseEntity.status(500).build();
         }
     }
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Livro> updateLivro(@PathVariable Integer id, @RequestBody Livro livroParaAtualizar){
+
+        try {
+            Optional<Livro> livroAchado = repository.findById(id);
+
+            if(livroAchado.isPresent()){
+                livroParaAtualizar.setId(id);
+                Livro livroAtualizado = repository.save(livroParaAtualizar);
+                return ResponseEntity.status(200).body(livroAtualizado);
+            }
+            return ResponseEntity.status(404).build();
+        }catch (Exception e){
+            return ResponseEntity.status(500).build();
+        }
+
+    }
+    @DeleteMapping("/remover/${id}")
+    public ResponseEntity deleteLivro(@PathVariable Integer id){
+        try{
+            Optional<Livro> livroAchado = repository.findById(id);
+
+            if(livroAchado.isPresent()){
+                repository.deleteById(id);
+                return ResponseEntity.status(200).build();
+            }
+            return ResponseEntity.status(404).build();
+
+        }catch (Exception e){
+            return ResponseEntity.status(500).build();
+        }
+    }
+
 }
