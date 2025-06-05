f6f541043e56d0f247018eb63378be89e983e14d
FelipeGasp <felipe.gasparotto@sptech.school>
2025-06-04 22:14:13 -0300

    fix:changed null verification to be first, avoiding null pointer

--- src/main/java/school/sptech/hub/validators/VendaValidator.java
+++ src/main/java/school/sptech/hub/validators/VendaValidator.java
@@ -7,7 +7,7 @@ import school.sptech.hub.entity.Venda;
 public class VendaValidator {
     public static boolean isValidVenda(Venda venda){
         int INVALID_TOTAL_RESERVA_QTD = 0;
-        if(venda.getTotalReserva() <= INVALID_TOTAL_RESERVA_QTD|| venda.getTotalReserva() == null) {
+        if(venda.getTotalReserva() == null || venda.getTotalReserva() <= INVALID_TOTAL_RESERVA_QTD) {
             return false;
         }
         return true;
