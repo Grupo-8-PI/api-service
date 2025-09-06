8f0048f3b4aab51f3b67f5f77a69bbe6583b9d6e
FelipeGasp <felipe.gasparotto@sptech.school>
2025-09-06 19:47:36 -0300

    fix: corrected errors in venda entity mapper

--- src/main/java/school/sptech/hub/infraestructure/gateways/venda/VendaEntityMapper.java
+++ src/main/java/school/sptech/hub/infraestructure/gateways/venda/VendaEntityMapper.java
@@ -2,37 +2,34 @@ package school.sptech.hub.infraestructure.gateways.venda;
 
 import school.sptech.hub.domain.entity.Venda;
 import school.sptech.hub.infraestructure.persistance.vendaPersistance.VendaEntity;
+import school.sptech.hub.infraestructure.gateways.usuario.UsuarioEntityMapper;
 
 public class VendaEntityMapper {
 
-    public VendaEntity toEntity(Venda venda) {
-        if (venda == null) {
-            return null;
-        }
+    public static VendaEntity toEntity(Venda venda) {
+        if (venda == null) return null;
 
         VendaEntity vendaEntity = new VendaEntity();
         vendaEntity.setId(venda.getId());
-        vendaEntity.setDataVenda(venda.getDataVenda());
-        vendaEntity.setQuantidade(venda.getQuantidade());
-        vendaEntity.setPrecoTotal(venda.getPrecoTotal());
-        vendaEntity.setUsuarios(venda.getUsuarios());
-        vendaEntity.setLivros(venda.getLivros());
+        vendaEntity.setDtReserva(venda.getDtReserva());
+        vendaEntity.setDtLimite(venda.getDtLimite());
+        vendaEntity.setStatusReserva(venda.getStatusReserva());
+        vendaEntity.setTotalReserva(venda.getTotalReserva());
+        vendaEntity.setUsuarios(UsuarioEntityMapper.toEntity(venda.getUsuarios()));
 
         return vendaEntity;
     }
 
-    public Venda toDomain(VendaEntity vendaEntity) {
-        if (vendaEntity == null) {
-            return null;
-        }
+    public static Venda toDomain(VendaEntity vendaEntity) {
+        if (vendaEntity == null) return null;
 
         Venda venda = new Venda();
         venda.setId(vendaEntity.getId());
-        venda.setDataVenda(vendaEntity.getDataVenda());
-        venda.setQuantidade(vendaEntity.getQuantidade());
-        venda.setPrecoTotal(vendaEntity.getPrecoTotal());
-        venda.setUsuarios(vendaEntity.getUsuarios());
-        venda.setLivros(vendaEntity.getLivros());
+        venda.setDtReserva(vendaEntity.getDtReserva());
+        venda.setDtLimite(vendaEntity.getDtLimite());
+        venda.setStatusReserva(vendaEntity.getStatusReserva());
+        venda.setTotalReserva(vendaEntity.getTotalReserva());
+        venda.setUsuarios(UsuarioEntityMapper.toDomain(vendaEntity.getUsuarios()));
 
         return venda;
     }
