5b356f9ebd0a6ec0f2003964779bccadbd857195
GabrielCerejo <gabriel.bellintani@sptech.school>
2025-09-08 11:34:02 -0300

    Refactor completo da camada Validator

--- src/main/java/school/sptech/hub/validators/UsuarioValidator.java
+++ src/main/java/school/sptech/hub/validators/UsuarioValidator.java
@@ -1,7 +1,29 @@
 package school.sptech.hub.validators;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
 public class UsuarioValidator {
-    public static boolean isValidUserType(String userType) {
-        return "admin".equalsIgnoreCase(userType) || "cliente".equalsIgnoreCase(userType);
+
+    public static final Logger logger = LoggerFactory.getLogger(UsuarioValidator.class);
+
+    public static final Set<String> TIPOS_VALIDOS = Set.of("ADMIN", "CLIENTE");
+
+    public static boolean isValidUserType(String userType){
+        if (userType == null || userType.trim().isEmpty()) {
+            logger.warn("Tentativa de cadastro com tipo de usuário nulo ou vazio");
+            return false;
+        }
+
+        String userTypeUpper = userType.trim().toUpperCase();
+        boolean isValid = TIPOS_VALIDOS.contains(userTypeUpper);
+
+        if (!isValid) {
+            logger.warn("Tentativa de uso de tipo de usuário inválido: {}", userType);
+        }
+
+        return isValid;
     }
 }
--- src/main/java/school/sptech/hub/validators/VendaValidator.java
+++ src/main/java/school/sptech/hub/validators/VendaValidator.java
@@ -2,14 +2,31 @@
 
 package school.sptech.hub.validators;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import school.sptech.hub.entity.Venda;
 
 public class VendaValidator {
-    public static boolean isValidVenda(Venda venda){
-        int INVALID_TOTAL_RESERVA_QTD = 0;
-        if(venda.getTotalReserva() == null || venda.getTotalReserva() <= INVALID_TOTAL_RESERVA_QTD) {
+
+    public static final Logger logger = LoggerFactory.getLogger(VendaValidator.class);
+    public static final int INVALID_TOTAL_RESERVA_QTD = 0;
+
+    public static boolean isValidTotalVenda(Venda venda){
+        if (venda == null) {
+            logger.warn("Tentativa de validação de venda nula.");
             return false;
         }
+
+        if (venda.getUsuarios() == null) {
+            logger.warn("Venda inválida: usuário não associado.");
+            return false;
+        }
+
+        if (venda.getTotalReserva() == null || venda.getTotalReserva() <= INVALID_TOTAL_RESERVA_QTD) {
+            logger.warn("Venda inválida: Valor total da reserva inválido. Valor recebido: {}", venda.getTotalReserva());
+            return false;
+        }
+
         return true;
     }
 }
