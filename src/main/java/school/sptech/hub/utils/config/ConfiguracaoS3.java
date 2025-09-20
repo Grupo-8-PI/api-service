8f0487372a49bb9ef846497a38fa1cc7e3229785
FelipeGasp <felipe.gasparotto@sptech.school>
2025-09-20 19:19:25 -0300

    feature: added s3 configuration

---
+++ src/main/java/school/sptech/hub/utils/config/ConfiguracaoS3.java
@@ -0,0 +1,21 @@
+package school.sptech.hub.utils.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class ConfiguracaoS3 {
+
+    @Value("${aws.regiao}")
+    private String awsRegion;
+
+    @Bean
+    public S3Client clienteS3() {
+        return S3Client.builder()
+                .region(Region.of(awsRegion))
+                .build();
+    }
+}
