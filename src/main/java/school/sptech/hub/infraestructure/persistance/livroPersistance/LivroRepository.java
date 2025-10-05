9914369874bcc73521527a4889878f069a48a4b5
FelipeGasp <felipe.gasparotto@sptech.school>
2025-10-05 18:29:29 -0300

    fix: corrupted files

--- src/main/java/school/sptech/hub/infraestructure/persistance/livroPersistance/LivroRepository.java
+++ src/main/java/school/sptech/hub/infraestructure/persistance/livroPersistance/LivroRepository.java
@@ -1,9 +1,21 @@
 package school.sptech.hub.infraestructure.persistance.livroPersistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<LivroEntity, Integer> {
     Optional<LivroEntity> findByIsbn(String isbn);
+
+    @Query("SELECT DISTINCT l.categoria.nomeCategoria FROM LivroEntity l ORDER BY l.categoria.nomeCategoria")
+    List<String> findAllDistinctCategorias();
+
+    @Query(value = "SELECT * FROM livro ORDER BY RAND() LIMIT 3", nativeQuery = true)
+    List<LivroEntity> findRecommendedRandomLivros();
+
+    @Query(value = "SELECT * FROM livro ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<LivroEntity> findRandomLivro();
 }
+
