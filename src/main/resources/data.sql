164e07fbb7814f95e1f09d4e87e484400a157c90
FelipeGasp <felipe.gasparotto@sptech.school>
2025-10-24 17:25:27 -0300

    fix: atualizado data.sql e dev properties para dados mais atualizados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,14 @@ INSERT IGNORE INTO acabamento (id, tipo_acabamento) VALUES (1, 'CAPA_DURA');
 INSERT IGNORE INTO acabamento (id, tipo_acabamento) VALUES (2, 'BROCHURA');
 
 
-INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Ficção');
-INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Não-ficção');
-
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Ficção Cientifica');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Romance');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Culinária');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Distopia');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Mistérios/Suspense');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Terror');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Filosofia');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Outros');
 
 -- Inserindo os 4 tipos fixos de conservação conforme o enum TipoConservacao
 INSERT IGNORE INTO conservacao (id, tipo_conservacao) VALUES (1, 'EXCELENTE');
