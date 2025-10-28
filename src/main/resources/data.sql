426ce0b154a712af1c10e7b85277d5410ccad077
FelipeGasp <felipe.gasparotto@sptech.school>
2025-10-27 23:22:15 -0300

    fix: added mysql compatibility in h2 database to enable ignore type insert

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 -- Inserindo os 2 tipos fixos de acabamento conforme o enum TipoAcabamento
-INSERT INTO acabamento (id, tipo_acabamento) VALUES (1, 'CAPA_DURA');
-INSERT INTO acabamento (id, tipo_acabamento) VALUES (2, 'BROCHURA');
+INSERT IGNORE INTO acabamento (id, tipo_acabamento) VALUES (1, 'CAPA_DURA');
+INSERT IGNORE INTO acabamento (id, tipo_acabamento) VALUES (2, 'BROCHURA');
 
 
-INSERT INTO categoria (nome_categoria) VALUES ('Ficção Cientifica');
-INSERT INTO categoria (nome_categoria) VALUES ('Romance');
-INSERT INTO categoria (nome_categoria) VALUES ('Culinária');
-INSERT INTO categoria (nome_categoria) VALUES ('Distopia');
-INSERT INTO categoria (nome_categoria) VALUES ('Mistérios/Suspense');
-INSERT INTO categoria (nome_categoria) VALUES ('Terror');
-INSERT INTO categoria (nome_categoria) VALUES ('Filosofia');
-INSERT INTO categoria (nome_categoria) VALUES ('Outros');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Ficção Cientifica');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Romance');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Culinária');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Distopia');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Mistérios/Suspense');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Terror');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Filosofia');
+INSERT IGNORE INTO categoria (nome_categoria) VALUES ('Outros');
 
 -- Inserindo os 4 tipos fixos de conservação conforme o enum TipoConservacao
-INSERT INTO conservacao (id, tipo_conservacao) VALUES (1, 'EXCELENTE');
-INSERT INTO conservacao (id, tipo_conservacao) VALUES (2, 'BOM');
-INSERT INTO conservacao (id, tipo_conservacao) VALUES (3, 'RAZOAVEL');
-INSERT INTO conservacao (id, tipo_conservacao) VALUES (4, 'DEGRADADO');
+INSERT IGNORE INTO conservacao (id, tipo_conservacao) VALUES (1, 'EXCELENTE');
+INSERT IGNORE INTO conservacao (id, tipo_conservacao) VALUES (2, 'BOM');
+INSERT IGNORE INTO conservacao (id, tipo_conservacao) VALUES (3, 'RAZOAVEL');
+INSERT IGNORE INTO conservacao (id, tipo_conservacao) VALUES (4, 'DEGRADADO');
