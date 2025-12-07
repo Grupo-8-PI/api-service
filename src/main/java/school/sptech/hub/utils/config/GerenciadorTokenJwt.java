94ad10e901de24e42239521a44f355f96e3e6f58
FelipeGasp <felipe.gasparotto@sptech.school>
2025-12-07 01:47:10 -0300

    fix: replace hardcoded JWT expiration time with configurable value

--- src/main/java/school/sptech/hub/utils/config/GerenciadorTokenJwt.java
+++ src/main/java/school/sptech/hub/utils/config/GerenciadorTokenJwt.java
@@ -44,7 +44,7 @@ public class GerenciadorTokenJwt {
                 .claim("id", usuarioDetalhes.getId())
                 .claim("roles", "ROLE_" + usuarioDetalhes.getTipoUsuario())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1800000
+                .setExpiration(new Date(System.currentTimeMillis() + jwtTokenValidity
                 ))
                 .signWith(parseSecret(), SignatureAlgorithm.HS512)
                 .compact();
